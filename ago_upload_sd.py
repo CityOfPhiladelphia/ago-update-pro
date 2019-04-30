fe79c2a4cc01246ea0d6e553d5ed4e6e1990e6cc
nicholas-fink-phl <16352920+nicholas-fink-phl@users.noreply.github.com>
2019-04-30 16:30:32 -0400

    Add files via upload

---
+++ ago_upload_sd.py
@@ -0,0 +1,88 @@
+from arcgis.gis import GIS
+import os
+import glob
+import multiprocessing
+from datetime import datetime
+from configparser import ConfigParser
+import logging
+import smtplib
+from email.mime.text import MIMEText
+
+config = ConfigParser()
+config.read('ago_upload_config.ini')
+
+# Logging variables
+MAX_BYTES = config.get('logging', 'max_bytes')  # in bytes
+# Max number appended to log files when MAX_BYTES reached
+BACKUP_COUNT = config.get('logging', 'file_count')
+
+# Create file logger (change logging level from default ERROR with the 'level' variable, i.e. DEBUG, INFO, WARN, ERROR, CRITICAL)
+logging.basicConfig(filename='ago_upload_log.txt', level=logging.INFO, format=('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger = logging.getLogger()
+
+
+def sendemail(sender, subject, text, recipientslist):
+    relay = config.get('email', 'relay')
+    commaspace = ', '
+    msg = MIMEText(text, 'html')
+    msg['To'] = commaspace.join(recipientslist)
+    msg['From'] = sender
+    msg['X-Priority'] = '2'
+    msg['Subject'] = subject
+    server = smtplib.SMTP(relay)
+    server.sendmail(sender, recipientslist, msg.as_string())
+    server.quit()
+
+
+def sd_update(sd):
+    name = sd.split('.')[0]
+    sdItem = gis.content.search("title:{} AND owner:{}".format(name, user), item_type="Service Definition")[0]
+    sdItem.update(data=sd)
+    print(name)
+
+    fs = sdItem.publish(overwrite=True)
+
+    if shrOrg or shrEveryone or shrGroups:
+        fs.share(org=shrOrg, everyone=shrEveryone, groups=shrGroups)
+
+    os.remove(sd)
+    print(str(name) + ' done.')
+
+# Set email alert options
+email_sender = config.get('email', 'sender')
+email_recipients = config.get('email', 'recipients').split(',')
+email_subject = 'AGO Update Failure'
+
+# Set AGO connection options
+portal = config.get('ago', 'portal')
+user = config.get('ago', 'user')
+password = config.get('ago', 'password')
+proxy = config.get('ago', 'proxy')
+
+# Set FS sharing options
+shrOrg = config.getboolean('ago', 'shrOrg')
+shrEveryone = config.getboolean('ago', 'shrEveryone')
+shrGroups = config.get('ago', 'shrGroups')
+
+# Get the list of service defintions to update in AGO
+service_definitions = glob.glob('*.sd')
+
+# Define the AGO organization to connect to
+gis = GIS(portal, user, password, proxy_host=proxy, proxy_port=8080)
+
+
+def main():
+    workers = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(workers)
+    pool.map(sd_update, service_definitions)
+    pool.close()
+    pool.join()
+
+if __name__ == '__main__':
+    start = datetime.now()
+    try:
+        main()
+    except Exception as e:
+        logger.error('AGO upload failed: ' + str(e))
+    finish = datetime.now() - start
+    logger.info('AGO upload complete in ' + str(finish))
---
+++ sd_export.py
@@ -0,0 +1,107 @@
+import arcpy
+import os
+import glob
+from arcgis.gis import GIS
+from configparser import ConfigParser
+import logging
+import smtplib
+from email.mime.text import MIMEText
+import socket
+
+config = ConfigParser()
+config.read('sd_export_config.ini')
+
+# Logging variables
+MAX_BYTES = config.get('logging', 'max_bytes')  # in bytes
+# Max number appended to log files when MAX_BYTES reached
+BACKUP_COUNT = config.get('logging', 'file_count')
+
+# Create file logger (change logging level from default ERROR with the 'level' variable, i.e. DEBUG, INFO, WARN, ERROR, CRITICAL)
+logging.basicConfig(filename='sd_export_log.txt', level=logging.INFO, format=('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger = logging.getLogger()
+
+
+def sendemail(sender, subject, text, recipientslist):
+    relay = config.get('email', 'relay')
+    commaspace = ', '
+    msg = MIMEText(text, 'html')
+    msg['To'] = commaspace.join(recipientslist)
+    msg['From'] = sender
+    msg['X-Priority'] = '2'
+    msg['Subject'] = subject
+    server = smtplib.SMTP(relay)
+    server.sendmail(sender, recipientslist, msg.as_string())
+    server.quit()
+
+
+def checks(mp):
+    # Get record count and schema of table to update
+    fc = mp.listLayers()[0].dataSource
+    count = int(arcpy.GetCount_management(fc)[0])
+    field_names = [f.name for f in arcpy.ListFields(fc)]
+    shapes = [f.name for f in arcpy.ListFields(fc, 'shape*')]
+    tbl_fields = sorted(set(field_names) - set(shapes))
+    # Get schema of table in AGO
+    fsItem = gis.content.search("title:{} AND owner:{}".format(mp.name, user), item_type="Feature Service")[0]
+    layer = fsItem.layers[0]
+    ago_fields = []
+    for f in layer.properties.fields:
+        if 'Shape' not in f['name']:
+            ago_fields.append(f['name'])
+    # If everything matches up, return True, else False
+    if sorted(ago_fields) == sorted(tbl_fields) and count > 0:
+        return True
+    else:
+        return False
+
+
+def create_sd(table):
+    # Create a new SDDraft and stage to SD
+    sddraft = str(table) + '.sddraft'
+    sd = str(table) + '.sd'
+    draft = mp.getWebLayerSharingDraft('HOSTING_SERVER', 'FEATURE', table)
+    draft.exportToSDDraft(sddraft)
+    arcpy.StageService_server(sddraft, sd)
+    os.remove(sddraft)
+
+# Set email alert options
+email_sender = config.get('email', 'sender')
+email_recipients = config.get('email', 'recipients').split(',')
+email_subject = 'AGO Update Failure'
+
+# Set AGO connection options
+portal = config.get('ago', 'portal')
+user = config.get('ago', 'user')
+password = config.get('ago', 'password')
+proxy = config.get('ago', 'proxy')
+
+# Set arcpy options
+arcpy.SetLogHistory(False)
+arcpy.env.workspace = os.getcwd()
+arcpy.env.overwriteOutput = True
+
+# Define where the .aprx files are located
+prjPath = config.get('local', 'prjPath')
+
+# Get a list of the .aprx files to stage the services
+aprx_files = glob.glob(prjPath)
+
+# Define the AGO organization to connect to
+gis = GIS(portal, user, password, proxy_host=proxy, proxy_port=8080)
+
+# Files have to be created sequentially because ArcGIS Pro doesn't like multiprocessing
+for aprx in aprx_files:
+    prj = arcpy.mp.ArcGISProject(aprx)
+    mp = prj.listMaps()[0]
+    try:
+        if checks(mp):
+            print(mp.name)
+            create_sd(mp.name)
+            logger.info('{} service definition successfully created.'.format(mp.name))
+        else:
+            logger.error('{} schema did not match or record count was 0.'.format(mp.name))
+            email_body = "{} service failed to update in ArcGIS Online. Please see the log for details on server {}.".format(mp.name, socket.gethostbyname(socket.gethostname()))
+    except Exception as e:
+        logger.error(mp.name, e)
+        email_body = "{} service failed to update in ArcGIS Online. Please see the log for details on server {}.".format(mp.name, socket.gethostbyname(socket.gethostname()))
+        continue
