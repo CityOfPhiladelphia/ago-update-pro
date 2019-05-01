8019a4986e4c195e955fe91e5ea314d286c57bc6
nicholas-fink-phl <16352920+nicholas-fink-phl@users.noreply.github.com>
2019-05-01 13:15:46 -0400

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,2 @@
+CALL "C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" D:\your\path\sd_export.py
+CALL "C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" D:\your\path\ago_upload_sd.py
